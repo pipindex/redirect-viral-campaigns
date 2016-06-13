c4853bfa55405e29059d14bae5f276dcfd92a009
Rakesh Kumar Nandi <rakeshk@SHAW-IN-90s-MacBook-Pro.local>
2016-06-13 13:12:54 +0530

    changes in structure and css file

--- src/social-channel-congratulations.php
+++ src/social-channel-congratulations.php
@@ -92,21 +92,25 @@ if (!empty($_COOKIE["Remind_Me_ShawAcademy"])) {
 				<section>
 					<div>
 						<h4>Select Course</h4>
-						<select class="form-control-rk">
-							<option class="desel" value="">Select Course</option>
-							<option value="alpha">alpha</option>
-							<option value="beta">beta</option>
-							<option value="gamma">gamma</option>
-						</select>
+						<div class="selectStyle" id="course">
+							<select class="form-control-rk" id="selectbox">
+								<option class="desel" value="" disabled selected>Select Course</option>
+								<option value="alpha">alpha</option>
+								<option value="beta">beta</option>
+								<option value="gamma">gamma</option>
+							</select>
+						</div>
 					</div>
 					<div>
 						<h4>Select Dates</h4>
-						<select class="form-control-rk">
-							<option class="desel" value="">Select Dates</option>
-							<option value="alpha">alpha</option>
-							<option value="beta">beta</option>
-							<option value="gamma">gamma</option>
-						</select>
+						<div class="selectStyle" id="dates">
+							<select class="form-control-rk">
+								<option class="desel" value="" disabled selected>Select Dates</option>
+								<option value="alpha">alpha</option>
+								<option value="beta">beta</option>
+								<option value="gamma">gamma</option>
+							</select>
+						</div>
 					</div>
 					<div>
 						<h4>Enter Password</h4>
@@ -139,6 +143,15 @@ if (!empty($_COOKIE["Remind_Me_ShawAcademy"])) {
 	</article>
 
 	<script>
+
+		
+
+			$("#course").on('change', function() {
+  				console.log('this started working');
+  				$("#dates").css('display','block');
+			});
+		
+
 		var reset = function() {
 	  		$("#phone").removeClass("error");
 	  		$("#error-msg").addClass("hide");
@@ -181,71 +194,6 @@ if (!empty($_COOKIE["Remind_Me_ShawAcademy"])) {
 				$("form").submit();
 			}
 		}
-
-		// Iterate over each select element
-		$('select').each(function () {
-
-		    // Cache the number of options
-		    var $this = $(this),
-		        numberOfOptions = $(this).children('option').length;
-
-		    // Hides the select element
-		    $this.addClass('s-hidden');
-
-		    // Wrap the select element in a div
-		    $this.wrap('<div class="select"></div>');
-
-		    // Insert a styled div to sit over the top of the hidden select element
-		    $this.after('<div class="styledSelect"></div>');
-
-		    // Cache the styled div
-		    var $styledSelect = $this.next('div.styledSelect');
-
-		    // Show the first select option in the styled div
-		    $styledSelect.text($this.children('option').eq(0).text());
-
-		    // Insert an unordered list after the styled div and also cache the list
-		    var $list = $('<ul />', {
-		        'class': 'options'
-		    }).insertAfter($styledSelect);
-
-		    // Insert a list item into the unordered list for each select option
-		    for (var i = 0; i < numberOfOptions; i++) {
-		        $('<li />', {
-		            text: $this.children('option').eq(i).text(),
-		            rel: $this.children('option').eq(i).val()
-		        }).appendTo($list);
-		    }
-
-		    // Cache the list items
-		    var $listItems = $list.children('li');
-
-		    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
-		    $styledSelect.click(function (e) {
-		        e.stopPropagation();
-		        $('div.styledSelect.active').each(function () {
-		            $(this).removeClass('active').next('ul.options').hide();
-		        });
-		        $(this).toggleClass('active').next('ul.options').toggle();
-		    });
-
-		    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
-		    // Updates the select element to have the value of the equivalent option
-		    $listItems.click(function (e) {
-		        e.stopPropagation();
-		        $styledSelect.text($(this).text()).removeClass('active');
-		        $this.val($(this).attr('rel'));
-		        $list.hide();
-		        /* alert($this.val()); Uncomment this for demonstration! */
-		    });
-
-		    // Hides the unordered list when clicking outside of it
-		    $(document).click(function () {
-		        $styledSelect.removeClass('active');
-		        $list.hide();
-		    });
-
-		});	
 	</script>
 </body>
 </html>

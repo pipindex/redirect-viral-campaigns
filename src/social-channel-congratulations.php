c377220c88c31db0b0b77cb551bddf69b9f74fc9
Rakesh Kumar Nandi <rakeshk@SHAW-IN-90s-MacBook-Pro.local>
2016-06-13 15:40:52 +0530

    styled error msg

--- src/social-channel-congratulations.php
+++ src/social-channel-congratulations.php
@@ -124,6 +124,7 @@ if (!empty($_COOKIE["Remind_Me_ShawAcademy"])) {
 						<h4><?= translateLabel('Re-enter Password',$translations) ?></h4>
 						<input class="form-control-rk" type="password" placeholder="<?= translateLabel('Re-enter Password',$translations) ?>">
 					</div>
+				</section>	
 					<input  type="hidden" name="product" value="<?php  echo $_GET['product']; ?>" >
 					<div class="errors-here">
 						<?php if (isset($errors['firstName'])): ?>
@@ -135,11 +136,9 @@ if (!empty($_COOKIE["Remind_Me_ShawAcademy"])) {
 						<?php if (isset($errors['email'])): ?>
 							<span ><?php echo $errors['email'] ?></span><br />
 						<?php endif; ?>
-
 						<span id="valid-msg" class="hide">✓ Valid</span>
 						<span id="error-msg" class="hide"><?= translateLabel('Invalid number',$translations) ?></span>
 					</div>
-				</section>
 				<br>
 				<a href="#" id="loger" class="btn" onclick="formSubmit()"><?= translateLabel('Submit',$translations) ?></a>
 			</form>
